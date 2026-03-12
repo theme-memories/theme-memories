e91cad7bc05c6bf48c64c7e6e163f9cdb4ac81fa
Amia33 <137411471+Amia33@users.noreply.github.com>
2026-03-12 13:28:28 +0000

    fix: Layout change

--- src/script/weather.js
+++ src/script/weather.js
@@ -75,14 +75,14 @@ export function updateWeatherWidget(widget, data) {
     html += `
       <div class="weather-content">
         <p>体感温度: ${temperature.feelsLike} °C</p>
-        <p>最低/最高温度: ${temperature.min} °C/${temperature.max} °C</p>
+        <p>最低/最高気温: ${temperature.min} °C / ${temperature.max} °C</p>
         <p>湿度: ${atmospheric.humidity} %&nbsp;&nbsp;気圧: ${atmospheric.pressure} hPa</p>
         <p>視程: ${atmospheric.visibility} m&nbsp;&nbsp;雲量: ${clouds} %</p>
-        <p>風: <span class="wind-arrow"></span> ${wind.speed} m/s&nbsp;&nbsp;瞬時: ${wind.gust} m/s</p>
-        <p>日の出/日の入り: ${sunriseTime}/${sunsetTime}</p>
-        ${rain ? `<p>雨量 (1h): ${rain} mm</p>` : ""}
-        ${snow ? `<p>雪量 (1h): ${snow} mm</p>` : ""}
-        <p>${updatedTime} 発表</p>
+        <p>風: <span class="wind-arrow"></span> ${wind.speed} m/s&nbsp;&nbsp;突風: ${wind.gust} m/s</p>
+        <p>日の出/日の入り: ${sunriseTime} / ${sunsetTime}</p>
+        ${rain ? `<p>降水量 (過去1時間): ${rain} mm</p>` : ""}
+        ${snow ? `<p>降雪量 (過去1時間): ${snow} mm</p>` : ""}
+        <p>観測時刻: ${updatedTime}</p>
       </div>
     `;
   }
