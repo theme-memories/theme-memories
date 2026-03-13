cbddbdc0ba8adf160726b2d0e17fa22ae00b4a38
Amia33 <137411471+Amia33@users.noreply.github.com>
2026-03-13 14:23:55 +0000

    chore: Split into 2 collections

--- src/content.config.ts
+++ src/content.config.ts
@@ -17,4 +17,19 @@ const blog = defineCollection({
     }),
 });
 
-export const collections = { blog };
+const kantei = defineCollection({
+  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  loader: glob({ base: "./src/content/kantei", pattern: "**/*.md" }),
+  // Type-check frontmatter using a schema
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      // Transform string to Date object
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: image().optional(),
+    }),
+});
+
+export const collections = { blog, kantei };
