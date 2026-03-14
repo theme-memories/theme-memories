75d8d7e04c51f110bb643970bf48eb5eb9ae2c8d
Amia33 <137411471+Amia33@users.noreply.github.com>
2026-03-14 11:20:07 +0000

    fix: Use meteorological wind speed

--- src/script/weather.js
+++ src/script/weather.js
@@ -78,7 +78,7 @@ export function updateWeatherWidget(widget, data) {
         <p>最低/最高気温: ${temperature.min} °C / ${temperature.max} °C</p>
         <p>湿度: ${atmospheric.humidity} %&nbsp;&nbsp;気圧: ${atmospheric.pressure} hPa</p>
         <p>視程: ${atmospheric.visibility} m&nbsp;&nbsp;雲量: ${clouds} %</p>
-        <p>風: <span class="wind-arrow"></span> ${wind.speed} m/s&nbsp;&nbsp;突風: ${wind.gust} m/s</p>
+        <p>風: <span class="wind-arrow">↑</span> ${wind.speed} m/s&nbsp;&nbsp;突風: ${wind.gust} m/s</p>
         <p>日の出/日の入り: ${sunriseTime} / ${sunsetTime}</p>
         ${rain ? `<p>降水量 (過去1時間): ${rain} mm</p>` : ""}
         ${snow ? `<p>降雪量 (過去1時間): ${snow} mm</p>` : ""}
@@ -92,7 +92,7 @@ export function updateWeatherWidget(widget, data) {
   if (widgetSize === "big") {
     const windArrow = widget.querySelector(".wind-arrow");
     if (windArrow) {
-      windArrow.style.transform = `rotate(${wind.direction}deg)`;
+      windArrow.style.transform = `rotate(${wind.direction + 180}deg)`;
     }
   }
 }
