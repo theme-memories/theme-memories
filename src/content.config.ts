83889cddced661f5980d9bb1d19958b093a84f78
Amia33 <137411471+Amia33@users.noreply.github.com>
2026-04-18 22:01:17 +0800

    chore: Use nodeCrypto

--- src/content.config.ts
+++ src/content.config.ts
@@ -58,7 +58,16 @@ const cpriv = defineCollection({
         path: ["protected"],
       }),
       question: z.string().optional().default(protectConfig.question),
-      password: z.string().optional().default(protectConfig.password),
+      password: z
+        .string()
+        .regex(
+          /[^a-zA-Z0-9!@#$%^&*]/,
+          "Format error, see README.md in submodule.",
+        )
+        .min(1, "Slug cannot be empty.")
+        .max(32, "Slug cannot exceed 32 characters.")
+        .optional()
+        .default(protectConfig.password),
     }),
 });
 
